8fb37b546edb4a28792177d1437ed8a343b058c5
Kimgcash <olugbamidayodavid88@gmail.com>
2023-06-15 01:24:56 -0700

    monty

--- main.c
+++ main.c
@@ -1,126 +1,47 @@
 #include "monty.h"
 
-extern_var_t ex;
+/* global struct to hold flag for queue and stack length */
+var_t var;
 
 /**
- * main - honestly, idk.
- * @argc: idk.
- * @argv: idk.
- * Return: less find out god.
+ * main - Monty bytecode interpreter
+ * @argc: number of arguments passed
+ * @argv: array of argument strings
+ *
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
  */
 int main(int argc, char *argv[])
 {
-	FILE *fp;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	FILE *fs = NULL;
+	char *lineptr = NULL, *op = NULL;
+	size_t n = 0;
 
-	/* arg error */
+	var.queue = 0;
+	var.stack_len = 0;
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	/* open file error */
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
+	fs = fopen(argv[1], "r");
+	if (fs == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	read_file(fp);
-	fclose(fp);
-	return (0);
-}
-
-/**
- * read_file - honestly, idk.
- * @fp: idk.
- */
-void read_file(FILE *fp)
-{
-	char *line = NULL;
-	size_t len = 0;
-	int i;
-	stack_t *top_ptr = NULL;
-
-	ex.fp = fp;
-
-	/* get new line from fp's buffer */
-	i = 1;
-	while (getline(&line, &len, fp) != -1)
+	on_exit(free_lineptr, &lineptr);
+	on_exit(free_stack, &stack);
+	on_exit(m_fs_close, fs);
+	while (getline(&lineptr, &n, fs) != -1)
 	{
-		if (not_empty(line))
+		line_number++;
+		op = strtok(lineptr, "\n\t\r ");
+		if (op != NULL && op[0] != '#')
 		{
-			ex.data = clean_spaces(line);
-			if (ex.data.value != NULL)
-			{
-				ex.int_value = atoi(ex.data.value);
-			}
-			ex.data.line_n = i;
-			ex.line = line;
-			execute_function(&top_ptr);
-		}
-		i++;
-	}
-	free(line);
-	free_stack(top_ptr);
-}
-
-/**
- * execute_function - honestly, idk.
- * @stack: idk.
- * Return: less find out god.
- */
-void execute_function(stack_t **stack)
-{
-	instruction_t functions[] = {
-		{"push", monty_push}, {"pall", monty_pall}, {"pint", monty_pint},
-		{"pop", monty_pop}, {"swap", monty_swap}, {"add", monty_add},
-		{"sub", monty_sub}, {"nop", monty_nop}, {"div", monty_div},
-		{"mul", monty_mul}, {"mod", monty_mod}, {"pchar", monty_pchar},
-		{"pstr", monty_pstr}, {NULL, NULL}
-	};
-	int i;
-
-	for (i = 0; functions[i].opcode; i++)
-	{
-		if (!strcmp(ex.data.op_func, functions[i].opcode))
-		{
-			functions[i].f(stack, ex.data.line_n);
-			break;
+			get_op(op, &stack, line_number);
 		}
 	}
-	if (!functions[i].opcode)
-	{
-
-		fprintf(stderr, "L%d: unknown instruction", ex.data.line_n);
-		fprintf(stderr, " %s\n", ex.data.op_func);
-		free(ex.line);
-		fclose(ex.fp);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-}
-/**
- * free_stack - honestly, idk.
- * @top_ptr: idk.
- * Return: less find out god.
- */
-void free_stack(stack_t *top_ptr)
-{
-	stack_t *last;
-	stack_t *prevNode;
-
-	last = top_ptr;
-
-	if (last)
-	{
-		prevNode = last->prev;
-
-		while (prevNode)
-		{
-			free(last);
-			last = prevNode;
-			prevNode = prevNode->prev;
-		}
-		free(last);
-	}
+	exit(EXIT_SUCCESS);
 }
